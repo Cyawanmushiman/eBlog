cb0532b2cfd90e8d54861e798b5a949c8a24505a
Cyawanmushiman <xiaochuanyingsi@gmail.com>
2022-06-07 14:17:53 +0900

    post/show post/category サイドバー作成 css適用

--- resources/views/post/categories.blade.php
+++ resources/views/post/categories.blade.php
@@ -1,24 +1,31 @@
 @extends('layouts.component')
 @section('content')
 {{Breadcrumbs::render('category',$category)}}
-<main class="home">
-  <div class="titleWrapper">
-    <h1 class="home__title">{{$category->name}}</h1>
-    <h2 class="home__title--japanese">{{$category->name}}の記事</h2>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+      <main class="home">
+        <div class="titleWrapper">
+          <h1 class="home__title">{{$category->name}}</h1>
+          <h2 class="home__title--japanese">{{$category->name}}の記事</h2>
+        </div>
+      
+        <div class="home__blogWrapper">
+          @if(count($posts) == 0)
+            <p>まだ投稿がありません</p>
+          @endif
+          @foreach($posts as $post)
+          <a href="{{route('post.show',$post)}}" class="BlogCard">
+            <span class="BlogCard__category">{{ $post->category->name }}</span>
+            <div class="BlogCard__image"><img src="{{asset('storage/eyeCatchImage/'.$post->eyeCatchImage)}}"
+                alt="eyeCatchImage"></div>
+            <h3 class="BlogCard__title">{{Str::limit($post->title,20,'...')}}</h3>
+          </a>
+          @endforeach
+        </div>
+      </main>
+    </div>
+    @include('layouts.sidebar')
   </div>
-
-  <div class="home__blogWrapper">
-    @if(count($posts) == 0)
-      <p>まだ投稿がありません</p>
-    @endif
-    @foreach($posts as $post)
-    <a href="{{route('post.show',$post)}}" class="BlogCard">
-      <span class="BlogCard__category">{{ $post->category->name }}</span>
-      <div class="BlogCard__image"><img src="{{asset('storage/eyeCatchImage/'.$post->eyeCatchImage)}}"
-          alt="eyeCatchImage"></div>
-      <h3 class="BlogCard__title">{{Str::limit($post->title,20,'...')}}</h3>
-    </a>
-    @endforeach
-  </div>
-</main>
+</div>
 @endsection
